6b7820f1d318a8cbaeef83d00d60880e7a46f2f9
Jose Seie <joseeugenioseie@hotmail.com>
2022-07-19 16:07:31 +0200

    chore: instalacao e configuracao de voyager

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+///admin/cursos
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
