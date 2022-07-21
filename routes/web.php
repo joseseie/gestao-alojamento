34980ac1944d942cd5284d10044b37b8e9641c48
Jose Seie <joseeugenioseie@hotmail.com>
2022-07-21 10:09:33 +0200

    feat: conclusao de CRUDs

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return redirect()->to('/admin');
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
